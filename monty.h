742d01f62f9116b3174977a92accb3024aea8d47
Angelo Garcia <64263025+angeloGarciaV@users.noreply.github.com>
2024-01-03 12:43:14 -0400

    Update monty.h

--- monty.h
+++ monty.h
@@ -43,5 +43,7 @@ typedef struct instruction_s
 /*Functions*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 
 #endif
